3d72619a3aeb484a431b766d2e45c6e1eb4d4814
shabnam <shabnamjm@code.edu.az>
2024-09-19 17:08:11 +0400

    product id page added

--- app/filter/page.tsx
+++ app/filter/page.tsx
@@ -1,10 +1,5 @@
-'use client'
-
-import { useState } from 'react'
 import {
-  Dialog as HeadlessUIDialog, // Aliasing the first Dialog import
-  DialogBackdrop,
-  DialogPanel,
+  Dialog as HeadlessUIDialog, //first Dialog import
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
@@ -19,7 +14,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import {
-    Dialog as CustomDialog, // Aliasing the second Dialog import
+    Dialog as CustomDialog, // second Dialog import
     DialogContent,
     DialogDescription,
     DialogFooter,
@@ -29,6 +24,9 @@ import {
   } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import prisma from '@/lib/db'
+import { Pencil, Trash } from 'lucide-react'
+
 
 const sortOptions = [
   { name: 'Most Popular', href: '#', current: true },
@@ -81,88 +79,16 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
-  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
-
+export default async function Example() {
+  const products = await prisma.product.findMany()
+  console.log(products);
+  
+  
+  
   return (
     <div className="bg-white font-serif mt-5">
       <div>
-        {/* Mobile filter dialog */}
-        <HeadlessUIDialog open={mobileFiltersOpen} onClose={setMobileFiltersOpen} className="relative z-40 lg:hidden">
-          <DialogBackdrop
-            transition
-            className="fixed inset-0 bg-black bg-opacity-25 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
-          />
 
-          <div className="fixed inset-0 z-40 flex">
-            <DialogPanel
-              transition
-              className="relative ml-auto flex h-full w-full max-w-xs transform flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl transition duration-300 ease-in-out data-[closed]:translate-x-full"
-            >
-              <div className="flex items-center justify-between px-4">
-                <h2 className="text-lg font-medium text-gray-900">Filters</h2>
-                <button
-                  type="button"
-                  onClick={() => setMobileFiltersOpen(false)}
-                  className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
-                >
-                  <span className="sr-only">Close menu</span>
-                  <XMarkIcon aria-hidden="true" className="h-6 w-6" />
-                </button>
-              </div>
-
-              {/* Filters */}
-              <form className="mt-4 border-t border-gray-200">
-                <h3 className="sr-only">Categories</h3>
-                <ul role="list" className="px-2 py-3 font-medium text-gray-900">
-                  {subCategories.map((category) => (
-                    <li key={category.name}>
-                      <a href={category.href} className="block px-2 py-3">
-                        {category.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-
-                {filters.map((section) => (
-                  <Disclosure key={section.id} as="div" className="border-t border-gray-200 px-4 py-6">
-                    <h3 className="-mx-2 -my-3 flow-root">
-                      <DisclosureButton className="group flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
-                        <span className="font-medium text-gray-900">{section.name}</span>
-                        <span className="ml-6 flex items-center">
-                          <PlusIcon aria-hidden="true" className="h-5 w-5 group-data-[open]:hidden" />
-                          <MinusIcon aria-hidden="true" className="h-5 w-5 [.group:not([data-open])_&]:hidden" />
-                        </span>
-                      </DisclosureButton>
-                    </h3>
-                    <DisclosurePanel className="pt-6">
-                      <div className="space-y-6">
-                        {section.options.map((option, optionIdx) => (
-                          <div key={option.value} className="flex items-center">
-                            <input
-                              defaultValue={option.value}
-                              defaultChecked={option.checked}
-                              id={`filter-mobile-${section.id}-${optionIdx}`}
-                              name={`${section.id}[]`}
-                              type="checkbox"
-                              className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                            />
-                            <label
-                              htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
-                              className="ml-3 min-w-0 flex-1 text-gray-500"
-                            >
-                              {option.label}
-                            </label>
-                          </div>
-                        ))}
-                      </div>
-                    </DisclosurePanel>
-                  </Disclosure>
-                ))}
-              </form>
-            </DialogPanel>
-          </div>
-        </HeadlessUIDialog>
 
         <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
@@ -298,31 +224,26 @@ export default function Example() {
               {/* Product grid */}
               <div className="lg:col-span-3">
 
-
-
-
-<section id="Projects"
-    className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-
-<div className="w-72 bg-white shadow-md rounded-xl duration-500">
-        
-           <div className='bg-[#F3E6DA] flex items-center justify-center'>
-           <Link href="/products">
-<Image src="https://lifestyle-eta.vercel.app/images/gogals-product-3.png" alt='img' width={200} height={150} className='pt-10  pb-10 text-center flex items-center justify-center'/>
-</Link>
-           </div>
-            <div className="px-4 py-3 w-72 flex items-center justify-between">
-                <p className="text-lg font-bold text-black truncate block capitalize">Product Name</p>
-                <div className="flex items-center">
-                    <p className="text-lg font-semibold text-black cursor-auto my-3">$149</p>
-                </div>
-            </div>
-    
-    </div>
-
-    
-
+<section id="Projects" className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+  {products.map((product) => (
+ <Link key={product.id} href={`/filter/product/${product.id}`}>
+      <div className="w-72 bg-white shadow-md rounded-xl duration-500">
+        <div className='bg-[#F3E6DA] h-[300px] flex items-center justify-center'>
+          <Image src={product.imageUrl} alt='img' width={200} height={150} className='pt-10 pb-10 text-center flex items-center justify-center' />
+        </div>
+        <div className="px-4 py-3 w-72 flex items-center justify-between bg-[#F3E6DA]">
+          <p className="text-lg font-bold text-black truncate block capitalize">{product.name}</p>
+          <p className="text-lg font-semibold text-black cursor-auto my-3">${product.price}</p>
+        </div>
+        <div className='px-4 py-1 w-72 flex items-center gap-2 bg-[#F3E6DA]'>
+          <Button className='bg-black hover:bg-green-600'><Pencil /></Button>
+          <Button className='bg-black hover:bg-red-600'><Trash /></Button>
+        </div>
+      </div>
+    </Link>
+  ))}
 </section>
+
               </div>
             </div>
           </section>
